4b31cd25088d6c206ccb9621f42e0ae3a0a75a5c
Salmane1991 <el.hamdouni.salmane@gmail.com>
2025-03-28 18:46:33 +0000

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,39 @@
-import React, { useState } from "react";
-import logo from "./logo.svg";
-import poweredBy from "./powered-by-vitawind-dark.png";
+import React, { useEffect, useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import axios from "axios";
 
-function App() {
-  const [count, setCount] = useState(0);
+const App = () => {
+  const [blogs, setBlogs] = useState([]);
+
+  // useEffect(() => {
+  //   axios.get("http://localhost:5000/api/blogs")
+  //     .then((response) => {
+  //       setBlogs(response.data);
+  //     })
+  //     .catch((error) => {
+  //       console.error("There was an error fetching the blogs:", error);
+  //     });
+  // }, []);
 
   return (
-    <div className="flex items-center justify-center h-screen">
-      geeks institute
+    <div className="container mt-5">
+      <h1 className="text-center">Blogging App</h1>
+      <div className="row">
+        {blogs.map((blog) => (
+          <div key={blog.id} className="col-md-4">
+            <div className="card mb-4">
+              <img src={blog.image} className="card-img-top" alt={blog.title} />
+              <div className="card-body">
+                <h5 className="card-title">{blog.title}</h5>
+                <p className="card-text">{blog.content.substring(0, 100)}...</p>
+                <a href="#" className="btn btn-primary">Read More</a>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
